0c5747d10143f03acb70bffca0b381a49e6106a5
Furkan Yaman <77271600+ffurkan04@users.noreply.github.com>
2025-07-21 22:28:21 +0300

    Program iskeleti yapıldı

--- task_controller/src/main/java/com/furkanyaman/Main.java
+++ task_controller/src/main/java/com/furkanyaman/Main.java
@@ -1,7 +1,31 @@
 package com.furkanyaman;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> taskList = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        boolean isWorking= true;
+        int number;
+        while(isWorking){
+            System.out.print("Sisteme hoşgelsiniz \n 1.Görev Ekle \n 2.Görev Sil \n 3.Görevleri Görüntüle \n 4.Çıkış");
+            number = input.nextInt();
+            switch(number){
+                case 1:
+                //görev ekle
+                case 2:
+                //görev sil
+                case 3:
+                //görevleri görüntüle
+                case 4:
+                //çıkış
+                default:
+                //hatalı veri
+            }
+        }
     }
 }
