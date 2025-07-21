fc5b5950bcc756b19fb38593f780d551104e7636
Furkan Yaman <77271600+ffurkan04@users.noreply.github.com>
2025-07-21 22:32:14 +0300

    Fonksiyonlar yazıldı

--- task_controller/src/main/java/com/furkanyaman/Main.java
+++ task_controller/src/main/java/com/furkanyaman/Main.java
@@ -5,27 +5,38 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    
+    static List<String> taskList = new ArrayList<>();
+    static Scanner input = new Scanner(System.in);
+    static boolean isWorking= true;
+    static int number;
     public static void main(String[] args) {
-        List<String> taskList = new ArrayList<>();
-        Scanner input = new Scanner(System.in);
-        boolean isWorking= true;
-        int number;
+        
         while(isWorking){
             System.out.print("Sisteme hoşgelsiniz \n 1.Görev Ekle \n 2.Görev Sil \n 3.Görevleri Görüntüle \n 4.Çıkış");
             number = input.nextInt();
             switch(number){
                 case 1:
-                //görev ekle
+                addTask();
+                
                 case 2:
-                //görev sil
+                removeTask();
                 case 3:
-                //görevleri görüntüle
+                showTask();
                 case 4:
-                //çıkış
+                isWorking =false;
+                break;
                 default:
-                //hatalı veri
+                System.out.println("Hatalı veri girildi");
             }
         }
+    }
+    public static void addTask(){
+        
+    }
+    public static void removeTask(){
+
+    }
+    public static void showTask(){
+
     }
 }
